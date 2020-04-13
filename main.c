e2fc536a3e4d0739c0534c53986963b8f68fad25
Olalekan Oyekunle <olaleksoyekunle@gmail.com>
2020-04-13 01:37:51 -0400

    Removed Floating point data from code

--- main.c
+++ main.c
@@ -21,7 +21,8 @@ uint_fast8_t Cache_block[1024][64] = {
 
 };
 
-void mem_write(uint32_t index, uint32_t block_offset, uint32_t data) {
+void mem_write(uint32_t index, uint32_t block_offset, uint32_t data)
+{
   // int i = 0;
   // //unpack 32 bit into 4 bytes, assume little endian
   // uint_fast8_t byte[4];
@@ -37,7 +38,8 @@ void mem_write(uint32_t index, uint32_t block_offset, uint32_t data) {
   // }
 }
 
-uint32_t mem_read(uint32_t index, uint32_t block_offset) {
+uint32_t mem_read(uint32_t index, uint32_t block_offset)
+{
   // uint32_t data;
   // uint_fast8_t byte[4];
   // int i = 0;
@@ -56,51 +58,66 @@ uint32_t mem_read(uint32_t index, uint32_t block_offset) {
   // return data;
 }
 
-void update_tag_table(uint32_t index, uint32_t tag_value) {
+void update_tag_table(uint32_t index, uint32_t tag_value)
+{
   tag_table_ptr[index] =
       (uint16_t)tag_value; // if a use the tag pointer here instead of the
                            // global array, it breaks
 }
 
-int compare_tag_value(uint32_t index, uint32_t tag_value) {
+int compare_tag_value(uint32_t index, uint32_t tag_value)
+{
   ++access_attempt;
 
   // if a use the tag pointer here instead of the global array, it breaks
-  if (tag_table_ptr[index] != (uint16_t)tag_value) {
+  if (tag_table_ptr[index] != (uint16_t)tag_value)
+  {
     ++cache_miss;
     return 0;
-  } else {
+  }
+  else
+  {
     ++cache_hit;
     return 1;
   }
 }
 
-uint32_t extract_placement_data(uint32_t address) {
+uint32_t extract_placement_data(uint32_t address)
+{
   uint32_t placement_value;
   placement_value = address & 0x000003ff;
   return placement_value;
 }
 
-uint32_t extract_offset(uint32_t address) {
+uint32_t extract_offset(uint32_t address)
+{
   uint32_t offset_value;
 
-  if (line_size == SIXTEEN_BYTES) {
+  if (line_size == SIXTEEN_BYTES)
+  {
     offset_value = address & 0x0000000f;
     return offset_value;
-  } else if (line_size == THIRTY_TWO_BYTES) {
+  }
+  else if (line_size == THIRTY_TWO_BYTES)
+  {
     offset_value = address & 0x0000001f;
     return offset_value;
-  } else if (line_size == SIXTY_FOUR_BYTES) {
+  }
+  else if (line_size == SIXTY_FOUR_BYTES)
+  {
     offset_value = address & 0x0000003f;
     return offset_value;
-  } else {
+  }
+  else
+  {
     offset_value = address & 0x0000007f;
     return offset_value;
   }
 }
 
 // function to extract the tag data value
-uint32_t extract_tag(uint32_t address) {
+uint32_t extract_tag(uint32_t address)
+{
   uint32_t tag_value;
   tag_value = address >> 16;
   tag_value = tag_value & 0x0000ffff;
@@ -108,29 +125,38 @@ uint32_t extract_tag(uint32_t address) {
 }
 
 // function to extract index data
-uint32_t extract_index(uint32_t address) {
+uint32_t extract_index(uint32_t address)
+{
   uint32_t index_value;
 
-  if (line_size == SIXTEEN_BYTES) {
+  if (line_size == SIXTEEN_BYTES)
+  {
     index_value = address >> 4;
     index_value = index_value & 0x00000fff;
     // return index_value;
-  } else if (line_size == THIRTY_TWO_BYTES) {
+  }
+  else if (line_size == THIRTY_TWO_BYTES)
+  {
     index_value = address >> 5;
     index_value = index_value & 0x000007ff;
     // return index_value;
-  } else if (line_size == SIXTY_FOUR_BYTES) {
+  }
+  else if (line_size == SIXTY_FOUR_BYTES)
+  {
     index_value = address >> 6;
     index_value = index_value & 0x000003ff;
     // return index_value;
-  } else {
+  }
+  else
+  {
     index_value = address >> 7;
     index_value = index_value & 0x000001ff;
     // return index_value;
   }
   // printf("index value = %d\n", index_value);
 
-  if (index_value >= 4096) {
+  if (index_value >= 4096)
+  {
     printf("BUGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG\n");
     while (1)
       ;
@@ -138,17 +164,20 @@ uint32_t extract_index(uint32_t address) {
   return index_value;
 }
 
-void process_record(record_t *record) {
+void process_record(record_t *record)
+{
   uint32_t tag_bits = extract_tag(record->address);
   uint32_t index_bits = extract_index(record->address);
 
   // if this return a zero, we have a miss
-  if (!compare_tag_value(index_bits, tag_bits)) {
+  if (!compare_tag_value(index_bits, tag_bits))
+  {
     // Update Cache block
     update_tag_table(index_bits, tag_bits);
   }
   access_type_t access_type = (access_type_t)record->access_type;
-  switch (access_type) {
+  switch (access_type)
+  {
   case READ:
     ++read_count;
     // data = mem_read(index_bits, offset_bits);
@@ -169,12 +198,13 @@ void process_record(record_t *record) {
   }
 }
 
-void print_simulation_result(void) {
+void print_simulation_result(void)
+{
   printf("No of reads = %d\n", read_count);
   printf("No of write = %d\n", write_count);
   printf("No of fetch = %d\n", instruction_count);
   printf("No of lines = %d\n", line_count);
-  printf("No of memory reference = %d\n", access_attempt);
+  printf("No of memory reference = %d\n", ++access_attempt);
   printf("No of Cache miss = %d\n", cache_miss);
   printf("No of Cache hits = %d\n", cache_hit);
   printf("Reseting result variables ..................................\n");
@@ -185,17 +215,20 @@ void print_simulation_result(void) {
          "=\n");
 }
 
-void allocate_tag_table_block(int size) {
+void allocate_tag_table_block(int size)
+{
   printf("Allocating %d table block in memory\n", size);
   tag_table_ptr = (uint16_t *)malloc(sizeof(uint16_t) * size);
 }
 
-void allocate_cahe_mem_block(int length, int width) {
+void allocate_cahe_mem_block(int length, int width)
+{
   printf("Allocating cache block in memory\n");
   cache_mem_block_ptr = (uint_fast8_t *)malloc(sizeof(length * width));
 }
 
-void run_simulation(FILE *fp, Cache_Line_size_t size) {
+void run_direct_mapped_simulation(FILE *fp, Cache_Line_size_t size)
+{
   printf("simulating cache of size %d KB, block size %d Bytes\n", 64, size);
 
   uint32_t line_data[3];
@@ -203,23 +236,42 @@ void run_simulation(FILE *fp, Cache_Line_size_t size) {
   int i = 0;
   line_size = size;
 
-  double no_of_cache_line_offset_bits = log2((double)line_size);
-  // printf("%f\n", no_of_cache_line_offset_bits);
-  double no_of_cache_line_index_bits =
-      (double)NO_OF_CACHE_LINE_BITS - no_of_cache_line_offset_bits;
-  // printf("%f\n", no_of_cache_line_index_bits);
-  double tag_table_allocation_size = pow(2, no_of_cache_line_index_bits);
-  // printf("%f\n", tag_table_allocation_size);
+  int no_of_cache_line_offset_bits = 0;
+  int no_of_cache_line_index_bits;
+  int tag_table_size = 1;
+  int memory_width = 1;
 
-  allocate_tag_table_block((int)tag_table_allocation_size);
-  allocate_cahe_mem_block((int)tag_table_allocation_size,
-                          (int)no_of_cache_line_offset_bits);
+  while (1)
+  {
+    /* code */
+    size = size >> 1;
+    no_of_cache_line_offset_bits++;
+    if (size == 1)
+      break;
+  }
+
+  no_of_cache_line_index_bits = NO_OF_CACHE_LINE_BITS - no_of_cache_line_offset_bits;
+
+  for (int i = 0; i < no_of_cache_line_index_bits; i++)
+  {
+    tag_table_size = tag_table_size << 1;
+  }
+
+  for (int i = 0; i < no_of_cache_line_offset_bits; i++)
+  {
+    memory_width = memory_width << 1;
+  }
+
+  allocate_tag_table_block(tag_table_size);
+  allocate_cahe_mem_block(tag_table_size, memory_width);
 
   record_t record;
-  while (1) {
+  while (1)
+  {
     fscanf(fp, "%x %x %x%*[\r\n]", &record.access_type, &record.address,
            &record.data);
-    if (feof(fp)) break;
+    if (feof(fp))
+      break;
     process_record(&record);
   }
   fclose(fp);
@@ -228,7 +280,8 @@ void run_simulation(FILE *fp, Cache_Line_size_t size) {
   print_simulation_result();
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[])
+{
 
   printf("The given argument is %s trace file\n", argv[1]);
   // char *filename = argv[1];
@@ -237,13 +290,20 @@ int main(int argc, char *argv[]) {
   fp = fopen("085.gcc.din.txt", "r");
 
   // Check if file exists
-  if (!fp) {
+  if (!fp)
+  {
     printf("Could not open sample.txt");
     return 0;
-  } else {
-
+  }
+  else
+  {
     printf("FILE OPENED\n");
-    run_simulation(fp, SIXTEEN_BYTES);
+    int size = CACHE_BLOCK_BASE_SIZE;
+    for (int i = 0; i < 4; i++)
+    {
+      run_direct_mapped_simulation(fp, size);
+      size = size << 1;
+    }
   }
 
   return 0;
--- main.h
+++ main.h
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <inttypes.h>
-#include <math.h>
 
 
 typedef enum access_type{
@@ -23,8 +22,10 @@ typedef struct record_s {
   uint32_t data;
 } record_t;
 
+
 #define NO_OF_RAM_BITS 32 // This is 4GB size of RAM
 #define NO_OF_CACHE_LINE_BITS 16 // Since Cache is 64kb
+#define CACHE_BLOCK_BASE_SIZE 16
 #define NO_OF_CACHE_TAG_BITS 16 
 
 
@@ -39,7 +40,7 @@ uint32_t mem_read(uint32_t index, uint32_t block_offset);
 void update_tag_table(uint32_t index, uint32_t tag_value);
 void print_simulation_result(void);
 
-void run_simulation(FILE * fp, Cache_Line_size_t size);
+void run_direct_mapped_simulation(FILE * fp, Cache_Line_size_t size);
 
 uint32_t extract_placement_data(uint32_t address);
 
